f12357dad8b5060d53cc137509c9bec7186a82cc
0day-Ashish <0day.ashish@gmail.com>
2026-07-10 02:08:40 +0530

    lenis scroll made faster

--- components/LenisProvider.tsx
+++ components/LenisProvider.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 
 export default function LenisProvider({ children }: { children: ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.15, duration: 0.8, smoothWheel: true }}>
       {children}
     </ReactLenis>
   );
